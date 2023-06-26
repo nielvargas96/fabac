fa37ef634f52089d54ce9cbfdb4cb82c32af8592
nielvargas96 <nielvargas96@gmail.com>
2023-06-26 08:47:28 +0800

    Initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Header from './Components/header';
+import Footer from './Components/footer';
+import Main from './Components/main';
+import Inner from './Components/inner';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Main />
+      {/* <Inner /> */}
+      <Footer />
+    </>
   );
 }
 
